e4d2ca5f1306c63c31da6ba44494c7bcee5a0fb5
superyngo <superyngo@gmail.com>
2023-08-07 15:43:31 +0800

    firstCommit

---
+++ src/App.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  import Snake from "./lib/snake.svelte";
+</script>
+
+<div class="wrapper">
+  <Snake canvasWidth={10} canvasHeight={10} speed={3} />
+</div>
+
+<style>
+  .wrapper {
+    position: relative;
+    height: 100vh;
+  }
+</style>
